c8913dfc7f21a1d9057edb5d97c74068c524bd60
js1 <jacob@slvr.mn>
2018-04-05 18:22:48 -0400

    finished google maps project

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const API_KEY = 'd477ca6db9c3619cf37ee70f53b285c0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},usn`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
